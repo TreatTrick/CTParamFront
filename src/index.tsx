844e978cc282889acd08823012b92b3d6e684509
TreatTrick <1154174767@qq.com>
2023-03-05 14:42:22 +0800

    调试路由成功

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './UI/Login';
+import {createBrowserRouter, redirect, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<SignIn/>,
+      children:[
+        {
+            path:"auth/",
+            children:[
+              {
+                index: true,
+                path:"login",
+                element:<SignIn/>,
+              }
+            ],
+        },
+      ],
+    },
+  ]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
