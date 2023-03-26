967b2ee15003a8888f4e16e127a51cbfdeb882a2
TreatTrick <1154174767@qq.com>
2023-03-26 17:56:49 +0800

    搜索取消功能

--- src/UI/dashboardChildren/searchBar.tsx
+++ src/UI/dashboardChildren/searchBar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 interface Props {
   onSearch: (searchTerm: string) => void;
@@ -9,23 +10,41 @@ interface Props {
 
 const SearchBar: React.FC<Props> = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
     onSearch(event.target.value);
+    if(event.target.value === ''){
+      setIsSearching(false);
+    }else{
+      setIsSearching(true);
+    }
+  };
+
+  const handleCancel = () => {
+    setSearchTerm('');
+    onSearch('');
+    setIsSearching(false);
   };
 
   return (
     <form noValidate autoComplete="off">
       <TextField id="outlined-basic" label="搜索" variant="standard" value={searchTerm} onChange={handleSearch} sx={{ width: '100%' }} InputProps={{
           endAdornment: (
-            <IconButton type="submit" aria-label="search">
-              <SearchIcon />
+            isSearching ?
+            <IconButton type="button" 
+            aria-label="search" 
+            onClick={()=>{ handleCancel() }}
+            >
+              {isSearching ? <CancelIcon /> : <SearchIcon />}
             </IconButton>
+            : null
           ),
         }} />
     </form>
   );
 };
+
 
 export default SearchBar;
